a7828621c5ae938e62a9cb48a07301af76e4cd0d
Chandan <singla12232@gmail.com>
2023-03-06 18:50:47 +0530

    final

---
+++ popup.js
@@ -0,0 +1,37 @@
+chrome.tabs.executeScript({
+    code: "window.getSelection().toString();"
+}, function(selection) {
+    const apiKey = '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'; // Replace with your actual API key
+    const endpoint = 'https://api.openai.com/v1/completions';
+
+    const prompt = `summarize this text and in case of comments write detailed analysis point wise: ${selection}`;
+
+    const data = {
+        "model": "text-davinci-003",
+        "prompt": prompt,
+        "temperature": 0,
+        "max_tokens": 1000,
+        "top_p": 1,
+        "frequency_penalty": 0,
+        "presence_penalty": 0
+    };
+
+    fetch(endpoint, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${apiKey}`
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        const summary = data.choices[0].text;
+        document.getElementById('summary').innerHTML = summary;
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+});
+
+  
